44e693374cb4153a6e6c37a895b0a639cb2c44e0
Alex <44402608+alexmgl@users.noreply.github.com>
2025-02-26 16:56:46 +0400

    Update config.py

--- data_tools/config.py
+++ data_tools/config.py
@@ -4,3 +4,6 @@ class GasConfig:
   GIE_AGSI_API = "https://agsi.gie.eu/"
   GIE_ALSI_API = "https://alsi.gie.eu/"
   GIE_IPP_API = "https://iip.gie.eu/"
+
+class PowerConfig:
+  raise NotImplementedError('Class not implemented')
