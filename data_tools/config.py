41af4c44be2456231fae24752c994b1d54cc6c0c
Alex <44402608+alexmgl@users.noreply.github.com>
2025-02-26 16:56:04 +0400

    Update config.py

--- data_tools/config.py
+++ data_tools/config.py
@@ -1,3 +1,6 @@
-GIE_AGSI_API = "https://agsi.gie.eu/"
-GIE_ALSI_API = "https://alsi.gie.eu/"
-GIE_IPP_API = "https://iip.gie.eu/"
+
+class GasConfig:
+
+  GIE_AGSI_API = "https://agsi.gie.eu/"
+  GIE_ALSI_API = "https://alsi.gie.eu/"
+  GIE_IPP_API = "https://iip.gie.eu/"
