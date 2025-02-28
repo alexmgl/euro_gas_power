f396f5e17dd94680f36f8a4e6196fce9cd3d4437
Alex <44402608+alexmgl@users.noreply.github.com>
2025-02-28 09:32:08 +0400

    Create elexon_gas.py

---
+++ src/gas/elexon.py
@@ -0,0 +1,4 @@
+# https://www.elexon.co.uk/data/open-settlement-data/
+
+# Central Data Collection Agent (CDCA) Data: Encompasses Inter Grid Supply Point Group Connection Period Data (IPD), Grid Supply Point Period Data (GP9), and more.
+# Settlement Administration Agent (SAA) Data: Includes Aggregate Party Day Charges (APC), Market Index Day Period Information (MD1), and other related datasets.
